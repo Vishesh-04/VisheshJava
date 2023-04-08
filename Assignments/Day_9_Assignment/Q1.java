dd1478717bd3ff5d5c9ac2dbec22bcaeb68d114d
Vishesh-04 <visheshjindal04@gmail.com>
2023-04-08 10:08:03 +0530

    Assignments

---
+++ Assignments/Day_9_Assignment/Q1.java
@@ -0,0 +1,11 @@
+class Q1{
+    public static void main(String args[]) {
+        int n =12;
+        for(int i = 0; i < n; i++){
+           for(int j = 0; j < n; j++){
+            System.out.print("* ");
+           }
+         System.out.println();
+        }
+    }
+}
